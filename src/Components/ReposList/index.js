c26bda390f9e467c25fb8b2d534aceb0cd41aab5
advance <advance@zhennandeMacBook-Pro.local>
2019-09-22 12:20:43 +0800

    save

--- src/Components/ReposList/index.js
+++ src/Components/ReposList/index.js
@@ -1,26 +1,35 @@
 import React, { memo } from 'react';
 import { View, Text, ScrollView, Image, Icon } from 'remax/wechat';
+import Colors from '@/utils/color';
 import styles from './index.module.less';
 
-const Item = ({ item }) => (
-  <View className={styles.listItem}>
-    <View className={styles.header}>
-      <View className={styles.header_left}>
-        <Image src={item.owner.avatar_url} />
-        <Text>{item.owner.login}</Text>
+const Item = ({ item }) => {
+  const obj = Colors[item.language]
+  if (!obj) {
+    return null
+  }
+
+  return (
+    <View className={styles.listItem}>
+      <View className={styles.header}>
+        <View className={styles.header_left}>
+          <Image src={item.owner.avatar_url} />
+          <Text>{item.owner.login}</Text>
+        </View>
+        <View className={styles.header_right}>
+          <View style={{ backgroundColor: obj.color }} className={styles.color}></View>
+          <Text>{item.language}</Text>
+        </View>
       </View>
-      <View>
-        {item.language}
+      <View className={styles.description}>{item.description ? item.description : 'No description provided'}</View>
+      <View className={styles.footer}>
+        <Text>☆ {item.stargazers_count}</Text>
+        <Text>1️⃣ {item.open_issues}</Text>
+        <Text>Forks {item.forks}</Text>
       </View>
     </View>
-    <View className={styles.description}>{item.description ? item.description : 'No description provided'}</View>
-    <View className={styles.footer}>
-      <Text>☆ {item.stargazers_count}</Text>
-      <Text>1️⃣ {item.open_issues}</Text>
-      <Text>Forks {item.forks}</Text>
-    </View>
-  </View>
-)
+  )
+}
 
 const ReposList = ({ data }) => {
   if (!data) {
@@ -32,7 +41,7 @@ const ReposList = ({ data }) => {
       scrollY={true}
     >
       {
-        data.map(item => <Item item={item} key={item.id}/>)
+        data.map(item => <Item item={item} key={item.id} />)
       }
     </ScrollView>
   )
